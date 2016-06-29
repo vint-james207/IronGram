30b9ca1d0ade7404f68f67beea7c40a8814dfa42
jamesyburr <vint.james@gmail.com>
2016-06-29 10:14:12 -0400

    logout updated

--- src/main/java/com/james/controllers/IronGramRestController.java
+++ src/main/java/com/james/controllers/IronGramRestController.java
@@ -66,6 +66,18 @@ public class IronGramRestController {
         }
          return photos.findByRecipient(user);
     }
+
+    @RequestMapping(path = "/user", method = RequestMethod.GET)
+    public User getUser(HttpSession session) {
+        String username = (String) session.getAttribute("username");
+        if (username == null) {
+            return null;
+        }
+        else {
+            return users.findFirstByName(username);
+        }
+    }
+
     @RequestMapping(path = "/public-photos", method = RequestMethod.GET)
     public Iterable<Photo> publicPhotos(String username) {
         User user = users.findFirstByName(username);
